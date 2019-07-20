5c8954f819f0adc66e7a9940dc5d90114db81038
Valentino Wong <valentino.wong@gmail.com>
2019-07-20 14:22:39 -0400

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,43 @@
 class Manager
 
+    # Manager#name returns a String that is the manager's name
+    # Manager#department returns a String that is the department that the manager oversees
+    # Manager#age returns a Fixnum that is the age of the manager
+    attr_reader :name, :department, :age
+
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    # Manager#employees returns an Array of all the employees that the manager oversees
+    def employees
+        Employee.all.select {|employee| employee.manager == self}
+    end
+
+    # Manager.all returns an Array of all the managers
+    def self.all
+        @@all
+    end
+
+    # Manager#hire_employee takes a String argument and a Fixnum argument of an employee's name and the employee's salary, respectively, and adds that employee to the list of employees that the manager oversees
+    def hire_employee(name, salary)
+        Employee.new(name, salary, self)
+    end
+
+    # Manager.all_departments returns an Array of all the department names that every manager oversees
+    def self.all_departments
+        Manager.all.map {|manager| manager.department}
+    end
+
+    #Manager.average_age returns a Float that is the average age of all the managers
+    def self.average_age
+        manager_ages = Manager.all.map {|manager| manager.age.to_f}
+        manager_ages.reduce {|total, sum| total + sum} / manager_ages.length
+    end
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,17 @@ require 'pry'
 
 #Test your code here
 
+#Managers
+michael = Manager.new("Michael Scott", "The Office", 40)
+richard = Manager.new("Richard Hendricks", "Pied Piper", 34)
+#Employees
+dwight = Employee.new("Dwight Schrute", 75000, michael)
+jim = Employee.new("Jim Halpert", 70000, michael)
+pam = Employee.new("Pam Beesly", 50000, michael)
+stanley = Employee.new("Stanley Hudson", 72000, michael)
+gilfoyle = Employee.new("Bertram Gilfoyle", 100000, richard)
+dinesh = Employee.new("Dinesh Chugtai", 100000, richard)
+jared = Employee.new("Jared Dunn", 95000, richard)
 
 binding.pry
 puts "done"
